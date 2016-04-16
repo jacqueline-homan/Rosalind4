20300961ca599f14e4398b43c262ea5801d2dc5d
Jacqueline Homan <jacquelinehoman7@gmail.com>
2016-04-16 06:10:26 -0400

    Solved using pattern-matching inside recursive function

--- Program.fs
+++ Program.fs
@@ -2,8 +2,34 @@
 open System.IO
 
 
+type TotalRabbitPopulation = {
+                               Breeders:bigint
+                               Babies:bigint }
+
+(*Creating an instance of breeders and babies 
+by binding a function, population, to a value.
+The 'p' needs to be bound to a value because 
+then we can use it later. 
+*)
+let population p = p.Breeders + p.Babies
+
+let rabbits n k =
+    let rec rabbitPop pop gen = 
+        match pop with
+        | _ when gen = n -> pop
+        | {Breeders= br; Babies = nb} -> rabbitPop {Breeders = br + nb; Babies = br * k} (gen + 1)
+    rabbitPop { Breeders = 0I; Babies = 1I} 1
+
+let total = rabbits 36 2I
+total
+|> printfn "%A"
+
+population total
+|> printfn "%A"
+
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    //printfn "%A" argv
     0 // return an integer exit code
 
